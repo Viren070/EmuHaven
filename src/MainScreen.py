942ae4eb4d71a5922ee4f9d582c053c895f456a1
Viren070 <viren070@protonmail.com>
2023-08-02 23:30:55 +0100

    properly check previous values and default values

--- src/MainScreen.py
+++ src/MainScreen.py
@@ -605,12 +605,17 @@ class MainScreen(customtkinter.CTk):    # create class
                 if "Temp\\_MEI" in previous_setting_value: # if Temp\\_MEI is in the previous setting value, then it means that it was from the -onefile exe and the path needs to be updated as it changes each time the app is opened
                     print_and_write_to_log(f"[{datetime.now().strftime('%H:%M:%S')}][CONSOLE] MainScreen.load_settings: Restoring {setting_name} to default as it uses old exe path")
                     self.restore_default_dolphin_settings(entry_id) # the default value will hold the new path 
-                if not os.path.exists(setting["var"].get()):
+                if not os.path.exists(previous_setting_value) and not os.path.exists(setting["default"]):
                     setting["var"].set("")
                     setting["entry"].delete(0, 'end')
                     setting["default"] = ""
+                elif not os.path.exists(previous_setting_value) and os.path.exists(setting["default"]):
+                    self.restore_default_dolphin_settings(entry_id)
                 else:
-                    setting["default"] = setting["var"].get()
+                    setting["default"] = previous_setting_value
+                    setting['var'].set(previous_setting_value)  # set the variable value to previous_value
+                    setting['entry'].delete(0, 'end')
+                    setting['entry'].insert(0, previous_setting_value)
                 continue # go to next setting
             setting['var'].set(previous_setting_value)  # set the variable value to previous_value 
             setting['entry'].delete(0, 'end')   # set the value of the entry widget to the previous value 
@@ -622,12 +627,17 @@ class MainScreen(customtkinter.CTk):    # create class
                 if "Temp\\_MEI" in previous_setting_value: # if Temp\\_MEI is in the previous setting value, then it means that it was from the -onefile exe and the path needs to be updated as it changes each time the app is opened
                     print_and_write_to_log(f"[{datetime.now().strftime('%H:%M:%S')}][CONSOLE] MainScreen.load_settings: Restoring {setting_name} to default as it uses old exe path")
                     self.restore_default_yuzu_settings(entry_id)
-                if not os.path.exists(setting["var"].get()):
+                if not os.path.exists(previous_setting_value) and not os.path.exists(setting["default"]):
                     setting["var"].set("")
                     setting["entry"].delete(0, 'end')
                     setting["default"] = ""
+                elif not os.path.exists(previous_setting_value) and os.path.exists(setting["default"]):
+                    self.restore_default_yuzu_settings(entry_id)
                 else:
-                    setting["default"] = setting["var"].get()
+                    setting["default"] = previous_setting_value
+                    setting['var'].set(previous_setting_value)  # set the variable value to previous_value
+                    setting['entry'].delete(0, 'end')
+                    setting['entry'].insert(0, previous_setting_value)
                 continue
             setting['var'].set(previous_setting_value)  # set the variable value to previous_value 
             setting['entry'].delete(0, 'end')   # set the value of the entry widget to the previous value 
