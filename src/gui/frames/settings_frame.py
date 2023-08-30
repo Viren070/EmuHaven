4b69856f10dc6c26e722b7f158b0d44ed0fc6c87
Viren070 <viren070@protonmail.com>
2023-08-30 16:38:44 +0100

    adjust padding

--- src/gui/frames/settings_frame.py
+++ src/gui/frames/settings_frame.py
@@ -29,12 +29,12 @@ class SettingsFrame(customtkinter.CTkFrame):
         self.app_settings_button = customtkinter.CTkButton(self.settings_navigation_frame, corner_radius=0, width=90, height=25, border_spacing=10, text="App",
                                                    fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
                                                    anchor="w", command=self.app_settings_button_event)
-        self.app_settings_button.grid(row=1, column=0, padx=2, sticky="ew")
+        self.app_settings_button.grid(row=1, column=0, padx=2, pady=(2,0), sticky="ew")
         
         self.dolphin_settings_button = customtkinter.CTkButton(self.settings_navigation_frame, corner_radius=0, width=100, height=25, border_spacing=10, text="Dolphin",
                                                    fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
                                                    anchor="w", command=self.dolphin_settings_button_event)
-        self.dolphin_settings_button.grid(row=2, column=0, padx=2, pady=(2,0), sticky="ew")
+        self.dolphin_settings_button.grid(row=2, column=0, padx=2, sticky="ew")
         
         self.yuzu_settings_button = customtkinter.CTkButton(self.settings_navigation_frame, corner_radius=0, width=100, height=25, border_spacing=10, text="Yuzu",
                                                    fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
