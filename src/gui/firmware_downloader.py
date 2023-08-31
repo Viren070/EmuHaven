d402876065c5be92aab4d866443b4729138a2339
Viren070 <viren070@protonmail.com>
2023-08-31 19:25:07 +0100

    add user agent

--- src/gui/firmware_downloader.py
+++ src/gui/firmware_downloader.py
@@ -867,7 +867,10 @@ class FirmwareDownloader(customtkinter.CTkFrame):
         filename = unquote(link.split("/")[-1])
 
         # link = "https://speed.hetzner.de/1GB.bin"
-        headers = {"Accept-Encoding": "identity"}  # Disable compression
+        headers = {
+            "Accept-Encoding": "identity", # Disable compression
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"
+            }
 
         try:
             session = requests.Session()
