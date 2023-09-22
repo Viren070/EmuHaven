163bcfd6e10e9b33048af16b1a05b5645588fa02
Viren070 <viren070@protonmail.com>
2023-09-22 17:09:39 +0100

    fix filetypes for custom dolphin

--- src/gui/frames/dolphin/dolphin_frame.py
+++ src/gui/frames/dolphin/dolphin_frame.py
@@ -170,7 +170,7 @@ class DolphinFrame(customtkinter.CTkFrame):
             return 
         path_to_archive = None
         if event.state & 1:
-            path_to_archive = PathDialog(filetypes=(".zip"), title="Custom Dolphin Archive", text="Type path to Dolphin Archive: ")
+            path_to_archive = PathDialog(filetypes=(".zip",), title="Custom Dolphin Archive", text="Type path to Dolphin Archive: ")
             path_to_archive = path_to_archive.get_input()
             if not all(path_to_archive):
                 if path_to_archive[1] is not None:
