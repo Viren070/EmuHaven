c1565dc3b1f295abc6c429fed2214fd90ad6ba50
Viren070 <viren070@protonmail.com>
2023-12-30 22:38:39 +0000

    add xenia settings frame to settings_frame.py

--- src/gui/frames/settings/settings_frame.py
+++ src/gui/frames/settings/settings_frame.py
@@ -6,6 +6,7 @@ from gui.frames.settings.app_settings_frame import AppSettingsFrame
 from gui.frames.settings.dolphin_settings_frame import DolphinSettingsFrame
 from gui.frames.settings.yuzu_settings_frame import YuzuSettingsFrame
 from gui.frames.settings.ryujinx_settings_frame import RyujinxSettingsFrame
+from gui.frames.settings.xenia_settings_frame import XeniaSettingsFrame
 
 class SettingsFrame(customtkinter.CTkFrame):
     def __init__(self, parent_frame, settings):
@@ -22,7 +23,7 @@ class SettingsFrame(customtkinter.CTkFrame):
         # create settings navigation frame
         self.settings_navigation_frame = customtkinter.CTkFrame(self, corner_radius=0, width=100, border_width=2)
         self.settings_navigation_frame.grid(row=0, column=0, sticky="nsew")
-        self.settings_navigation_frame.grid_rowconfigure(5, weight=1)
+        self.settings_navigation_frame.grid_rowconfigure(10, weight=1)
 
         # create settings navigation menu buttons
         text_color = ThemeManager.theme["CTkLabel"]["text_color"]
@@ -46,10 +47,16 @@ class SettingsFrame(customtkinter.CTkFrame):
                                                                anchor="w", command=self.ryujinx_settings_button_event)
         self.ryujinx_settings_button.grid(row=4, column=0, padx=2, sticky="ew")
 
+        self.xenia_settings_button = customtkinter.CTkButton(self.settings_navigation_frame, corner_radius=0, width=100, height=25, border_spacing=10, text="Xenia",
+                                                                fg_color="transparent", text_color=text_color,
+                                                                anchor="w", command=self.xenia_settings_button_event)
+        self.xenia_settings_button.grid(row=5, column=0, padx=2, sticky="ew")
+
         self.dolphin_settings_frame = DolphinSettingsFrame(self, self.settings)
         self.yuzu_settings_frame = YuzuSettingsFrame(self, self.settings)
         self.ryujinx_settings_frame = RyujinxSettingsFrame(self, self.settings)
         self.app_settings_frame = AppSettingsFrame(self, self.settings)
+        self.xenia_settings_frame = XeniaSettingsFrame(self, self.settings)
 
     def dolphin_settings_button_event(self):
         self.select_settings_frame_by_name("dolphin")
@@ -60,6 +67,9 @@ class SettingsFrame(customtkinter.CTkFrame):
     def ryujinx_settings_button_event(self):
         self.select_settings_frame_by_name("ryujinx")
 
+    def xenia_settings_button_event(self):
+        self.select_settings_frame_by_name("xenia")
+
     def app_settings_button_event(self):
         self.select_settings_frame_by_name("app")
 
@@ -69,6 +79,8 @@ class SettingsFrame(customtkinter.CTkFrame):
         self.dolphin_settings_button.configure(fg_color=self.dolphin_settings_button.cget("hover_color") if name == "dolphin" else "transparent")
         self.ryujinx_settings_button.configure(fg_color=self.ryujinx_settings_button.cget("hover_color") if name == "ryujinx" else "transparent")
         self.app_settings_button.configure(fg_color=self.app_settings_button.cget("hover_color") if name == "app" else "transparent")
+        self.xenia_settings_button.configure(fg_color=self.xenia_settings_button.cget("hover_color") if name == "xenia" else "transparent")
+
         # show selected frame
         if name == "dolphin":
             self.dolphin_settings_frame.grid(row=0, column=1, sticky="nsew")
@@ -82,6 +94,10 @@ class SettingsFrame(customtkinter.CTkFrame):
             self.ryujinx_settings_frame.grid(row=0, column=1, sticky="nsew")
         else:
             self.ryujinx_settings_frame.grid_forget()
+        if name == "xenia":
+            self.xenia_settings_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.xenia_settings_frame.grid_forget()
         if name == "app":
             self.app_settings_frame.grid(row=0, column=1, sticky="nsew")
         else:
